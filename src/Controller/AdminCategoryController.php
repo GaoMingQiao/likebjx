481206ff660838f1992c872b83a3294c56680599
WeiweiPx <weiwei.liu@nbmgroupe.com>
2023-06-29 12:05:46 +0200

    reomve route admin catalogue

--- src/Controller/AdminCategoryController.php
+++ src/Controller/AdminCategoryController.php
@@ -21,15 +21,6 @@ class AdminCategoryController extends AbstractController
         ]);
     }
 
-    #[Route('/catalogue', name: 'catalogue', methods: ['GET'])]
-    public function showCatalogue(CategoryRepository $categoryRepository): Response
-    {
-        return $this->render('base.html.twig', [
-            'categories' => $categoryRepository->findAll(),
-        ]);
-    }
-
-
     #[Route('/new', name: 'app_admin_category_new', methods: ['GET', 'POST'])]
     public function new(Request $request, CategoryRepository $categoryRepository): Response
     {
